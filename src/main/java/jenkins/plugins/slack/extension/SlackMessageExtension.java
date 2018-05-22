bde779054310f404659768944e2674ff51f8adea
caguilar187 <caguilar187@gmail.com>
2018-05-22 17:17:29 -0400

    Use run instead of build to be more generic

--- src/main/java/jenkins/plugins/slack/extension/SlackMessageExtension.java
+++ src/main/java/jenkins/plugins/slack/extension/SlackMessageExtension.java
@@ -1,11 +1,11 @@
 package jenkins.plugins.slack.extension;
 
 import hudson.ExtensionPoint;
-import hudson.model.AbstractBuild;
+import hudson.model.Run;
 
 public abstract class SlackMessageExtension implements ExtensionPoint {
 
-    public String doReplacement(String message, AbstractBuild build) {
+    public String doReplacement(String message, Run build) {
         return message;
     }
 
